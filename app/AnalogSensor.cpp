2a97f4c1cbc38606e16d07019ecd2a679f270f0f
Sayan Brahma <sbrahma0@gmail.com>
2019-10-02 14:53:15 -0400

    Adding all the fixed files in the app folder

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -1,22 +1,27 @@
-#include <AnalogSensor.hpp>
+/**
+ *  @file    AnalogSensor.cpp
+ *  @author  Sayan Brahma
+ *  @copyright Sayan Brahma
+ *  @note Valgrind exercise enpm808x
+ *  @brief Edited the file to fix the bugs reported by Valgrind tool
+ */
 #include <numeric>
 #include <vector>
+#include <AnalogSensor.hpp>
+
 
 AnalogSensor::AnalogSensor(unsigned int samples)
-    : mSamples(samples)
-{
+    : mSamples(samples) {
 }
 
-AnalogSensor::~AnalogSensor()
-{
+AnalogSensor::~AnalogSensor() {
 }
 
-int AnalogSensor::Read()
-{
+int AnalogSensor::Read() {
     std::vector<int> *readings = new std::vector<int>(mSamples, 10);
 
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+    double result = std::accumulate(readings->begin(), readings->end(), 0.0) / readings->size();
+    delete readings;  // Fix 2 - deleting the reading variable after being used
     return result;
 }
-
 
--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,18 @@
+/**
+ *  @file    main.cpp
+ *  @author  Sayan Brahma
+ *  @copyright Sayan Brahma
+ *  @note Valgrind exercise enpm808x
+ *  @brief Edited the file to fix the bugs reported by Valgrind tool
+ */
 #include <iostream>
 #include <AnalogSensor.hpp>
 
-int main()
-{
+int main() {
     AnalogSensor lightSensor(5);
     std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
-    bool terminator;
-    if( terminator )
-    {
+    bool terminator = true;  // Fix 1 by initializing the boolean variable
+    if ( terminator ) {
         std::cout << "DONE" << std::endl;
     }
     return 0;
