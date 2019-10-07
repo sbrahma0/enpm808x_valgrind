dede989197048fe31eda1e29cbb8f748bfe76006
Sayan Brahma <sbrahma0@gmail.com>
2019-10-06 20:42:26 -0400

    Adding 1 test case

--- test/test.cpp
+++ test/test.cpp
@@ -6,8 +6,10 @@
  *  @brief Added comments
  */
 #include <gtest/gtest.h>
+#include"AnalogSensor.hpp"
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+AnalogSensor test(10);
+TEST(AnalogSensorTest, Read_Test) {
+  EXPECT_EQ(10, test.Read());
 }
 
